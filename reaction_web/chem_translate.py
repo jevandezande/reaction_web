c24b73160d8e164ca2ef46a5857b67bee2f12f93
Jonathon Vandezande <jevandezande@gmail.com>
2022-03-09 14:59:15 -0500

    Adds UnicodeConvertor.

--- reaction_web/chem_translate.py
+++ reaction_web/chem_translate.py
@@ -23,14 +23,21 @@ def get_num(string: str) -> str:
 
 
 def translate(string: str, to: str = "latex") -> str:
+    to = to.lower()
     if to == "latex":
         return LatexConvertor.convert(string)
+    elif to == "unicode":
+        return UnicodeConvertor.convert(string)
     else:
         raise ValueError(f"{to=} is not a supported translation")
 
 
 class Convertor(ABC):
-    OPERATIONS = ["+", "=", ";", "->", "<-", "<>"]
+    """
+    Converts a chemical formula into the desired format
+    """
+
+    OPERATIONS = {"+", "=", ";", "->", "<-", "<>"}
 
     @classmethod
     def convert(cls, string: str) -> str:
@@ -49,7 +56,7 @@ class Convertor(ABC):
     @classmethod
     def mol_convertor(cls, string: str) -> str:
         """
-        Converts a molecular formula to latex
+        Converts a molecular formula to the desired format
         >>> LatexConvertor.mol_convertor("H2O")
         'H$_2$O'
         >>> LatexConvertor.mol_convertor("(NH4)(PO4)^2-")
@@ -136,3 +143,37 @@ class LatexConvertor(Convertor):
         '$^+$'
         """
         return f"$^{{{number}{charge}}}$" if number else f"$^{charge}$"
+
+
+class UnicodeConvertor(Convertor):
+    """
+    Converts a string to unicode
+    >>> UnicodeConvertor.convert("H2O + NH3 -> OH- + NH4+")
+    'H₂O + NH₃ -> OH⁻ + NH₄⁺'
+    """
+
+    @classmethod
+    def subscript_number(cls, number: str) -> str:
+        """
+        Converts a number to subscript
+        >>> UnicodeConvertor.subscript_number("123")
+        '₁₂₃'
+        """
+        return str.translate(number, UNICODE_SUBSCRIPT_TRANSLATION)
+
+    @classmethod
+    def superscript_number_charge(cls, number: str, charge: str) -> str:
+        """
+        Converts a number and charge to superscript
+        >>> UnicodeConvertor.superscript_number_charge("123", "-")
+        '¹²³⁻'
+        >>> UnicodeConvertor.superscript_number_charge("", "+")
+        '⁺'
+        """
+        out = str.translate(number, UNICODE_SUPERSCRIPT_TRANSLATION) if number else ""
+        return out + str.translate(charge, UNICODE_CHARGES_TRANSLATION)
+
+
+UNICODE_SUPERSCRIPT_TRANSLATION = {ord(str(i)): v for i, v in enumerate("⁰¹²³⁴⁵⁶⁷⁸⁹")}
+UNICODE_SUBSCRIPT_TRANSLATION = {ord(str(i)): v for i, v in enumerate("₀₁₂₃₄₅₆₇₈₉")}
+UNICODE_CHARGES_TRANSLATION = {ord("+"): "⁺", ord("-"): "⁻"}
