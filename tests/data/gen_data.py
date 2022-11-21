92f2634b1fb0c9fc73073f2ef3186edd645ca3b1
Jonathon Vandezande <jevandezande@gmail.com>
2022-11-21 10:07:00 -0500

    Fixes pytest options section name.

--- tests/data/gen_data.py
+++ tests/data/gen_data.py
@@ -1,4 +1,8 @@
+import random
 from itertools import product
+from string import ascii_lowercase, ascii_uppercase
+
+from more_itertools import take
 
 
 def gen_enumeration():
@@ -15,9 +19,9 @@ def gen_enumeration():
     data = "".join(
         f"""\
 A, 1, {r1}, {r2}, {r3}, {r4}, {r5}, {e1}
-B, 1, {r1}, {r2}, {r3}, {r4}, {r5}, {e2}
-C, 1, {r1}, {r2}, {r3}, {r4}, {r5}, {e3}
-D, 1, {r1}, {r2}, {r3}, {r4}, {r5}, {e4 + e5}
+B, 2, {r1}, {r2}, {r3}, {r4}, {r5}, {e2}
+C, 3, {r1}, {r2}, {r3}, {r4}, {r5}, {e3}
+D, 4, {r1}, {r2}, {r3}, {r4}, {r5}, {e4 + e5}
 """
         for (r1, e1), (r2, e2), (r3, e3), (r4, e4), (r5, e5) in product(*r_groups.values())
     )
@@ -25,6 +29,35 @@ D, 1, {r1}, {r2}, {r3}, {r4}, {r5}, {e4 + e5}
     return header + data.strip()
 
 
+def gen_enumeration_from_shape(r_shape: tuple[int, ...], num_steps: int = 5, seed: int | None = None):
+    """
+    Programmatically generate an enumeration for testing purposes.
+
+    :param r_shape: shape of the r_groups
+    :param num_steps: number of steps in the Paths
+    :param seed: random seed for energies
+    """
+    if seed:
+        random.seed(seed)
+
+    header = "name, step, " + ", ".join(f"r{i}" for i in range(len(r_shape))) + ", energy\n"
+
+    molecules = ascii_lowercase[:num_steps]
+    r_group_it = iter(ascii_uppercase)
+    r_groups = [take(size, r_group_it) for size in r_shape]
+
+    return header + "\n".join(
+        "\n".join(
+            f"{mol}, {step}, " + ", ".join(r_group_set) + f", {random.randint(0, 10)}"
+            for step, mol in enumerate(molecules)
+        )
+        for r_group_set in product(*r_groups)
+    )
+
+
 if __name__ == "__main__":
     with open("enum_2_3_2_3_4.csv", "w") as f:
         f.write(gen_enumeration())
+
+    with open("enum_3_4_3.csv", "w") as f:
+        f.write(gen_enumeration_from_shape((3, 4, 3), num_steps=4, seed=42))
